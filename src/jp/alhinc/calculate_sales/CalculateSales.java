c3aa3968ad37ad09f9f37b82293c95eb381fd2eb
hotogi_shoya <hotogi.shoya@alhinc.jp>
2026-02-06 17:07:12 +0900

    2/6commit

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CalculateSales {
@@ -37,7 +39,73 @@ public class CalculateSales {
 		}
 
 		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
+		//listFilesを使⽤してfilesという配列に、
+		//指定したパスに存在する全てのファイル(または、ディレクトリ)の情報を格納します。
+		File[] files = new File("C:\\Users\\trainee1469\\Desktop\\売上集計システム課題").listFiles();
 
+		//ファイルの情報を格納する List(ArrayList)
+		List<File> rcdFiles = new ArrayList<File>();
+
+		for(int i = 0; i < files.length ; i++) {
+			//ファイル名が取得
+			//matches を使用してファイル名が「数字8桁.rcd」なのか判定。
+			if(files[i].getName() .matches("^[0-9]{8}\\.rcd$")) {
+				rcdFiles.add(files[i]);
+			}
+		}
+//		System.out.println(rcdFiles);
+
+
+		for(int i = 0; i < rcdFiles.size(); i++) {
+
+			BufferedReader br = null;
+
+			String branch = "";
+			String sales = "";
+			//支店定義ファイル読み込み(readFileメソッド)を参考に売上ファイルの中身を読み込みます。
+			//売上ファイルの1行目には支店コード、2行目には売上金額が入っています。
+			try {
+				FileReader fr = new FileReader(rcdFiles.get(i));
+				br = new BufferedReader(fr);
+
+				String line;
+				int count = 1;
+				// 一行ずつ読み込む
+				while((line = br.readLine()) != null) {
+//					System.out.println(line);
+					//1行目=支店コード、2行目=金額
+					if (count == 1) {
+						branch = line;
+					} else if (count == 2) {
+						sales = line;
+					}
+					count++;
+				}
+			} catch(IOException e) {
+				System.out.println(UNKNOWN_ERROR);
+			} finally {
+				// ファイルを開いている場合
+				if(br != null) {
+					try {
+						// ファイルを閉じる
+						br.close();
+					} catch(IOException e) {
+						System.out.println(UNKNOWN_ERROR);
+					}
+				}
+			}
+
+			//売上ファイルから読み込んだ売上金額をMapに加算していくために、型の変換を行います。
+			//※詳細は後述で説明
+			long fileSale = Long.parseLong(sales);
+			//読み込んだ売上⾦額を加算します。
+			//※詳細は後述で説明
+			Long saleAmount = branchSales.get(branch) + fileSale;
+
+			//加算した売上⾦額をMapに追加します。
+			branchSales.put(branch, saleAmount);
+		}
+		System.out.println(branchSales);
 
 
 		// 支店別集計ファイル書き込み処理
@@ -69,6 +137,10 @@ public class CalculateSales {
 			while((line = br.readLine()) != null) {
 				// ※ここの読み込み処理を変更してください。(処理内容1-2)
 				System.out.println(line);
+				//⽀店定義ファイル格納
+				String[] items = line.split(",");
+			    branchNames.put(items[0], items[1]);
+			    branchSales.put(items[0], 0L);
 			}
 
 		} catch(IOException e) {
